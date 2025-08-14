cfcdb36828678ad175ace9cbff22468627b6ba29
Tiamat <Jaycruz718@gmail.com>
2025-08-14 14:46:01 -0400

    Finished our lesson

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-let form = document.querySelector('form');
+let form = document.querySelector("form");
+let username = document.getElementsByName("Username");
+let age = document.getElementsByName("age");
+let pw = document.getElementsByName("password");
+let confirm = document.getElementById('Confirm');
+// console.log(username[0], age[0], email[0], pw[0]);
 
 function handleSubmit(e){
-    e.preventDefault();
-    console.log('Event Firing')
+    e.preventDefault
+    // console.log('Event Firing')
 }
 
-form.addEventListener('change', handleSubmit)
+form.addEventListener('input', handleSubmit)
+
+function validate(e) {
+    let usrValid = validUsername();
+}
+
+function validUsername(){
+    if (username[0].value == "")
+        alert("Please enter a username");
+        return false;
+    }
+
+    return username.value;
+
+function validAge(){
+    if (user)
+}
+
