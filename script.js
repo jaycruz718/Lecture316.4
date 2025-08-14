d35d76daa5e909d8b3b41368205e99e7f07dc377
Tiamat <Jaycruz718@gmail.com>
2025-08-14 13:09:09 -0400

    JS Form Validation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('')
+let form = document.querySelector('form');
+
+function handleSubmit(e){
+    e.preventDefault();
+    console.log('Event Firing')
+}
+
+form.addEventListener('change', handleSubmit)
